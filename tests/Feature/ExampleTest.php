ee3af379de857bd9ec309614ee2f07bb284847ff
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-07-06 23:04:07 +0800

    fix feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,31 +1,14 @@
 <?php
-<<<<<<< HEAD
-namespace Tests\Feature;
-=======
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
->>>>>>> parent of 836c162 (fix remove tests)
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-<<<<<<< HEAD
     public function test_example()
     {
         $response = $this->get('/');
         $response->assertStatus(200);
     }
-=======
-    /**
-     * A basic test example.
-     */
-    // public function test_the_application_returns_a_successful_response(): void
-    // {
-    //     $response = $this->get('/');
-
-    //     $response->assertStatus(200);
-    // }
->>>>>>> parent of 836c162 (fix remove tests)
 }
