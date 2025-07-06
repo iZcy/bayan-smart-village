63b77a1d54266147235bf26e8311b8b438529b4c
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-07-06 23:02:16 +0800

    fix merge revert

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,27 +1,13 @@
 <?php
-<<<<<<< HEAD
-namespace Tests\Unit;
-=======
 
 namespace Tests\Unit;
 
->>>>>>> parent of 836c162 (fix remove tests)
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-<<<<<<< HEAD
     public function test_example()
     {
         $this->assertTrue(true);
     }
-=======
-    /**
-     * A basic test example.
-     */
-    // public function test_that_true_is_true(): void
-    // {
-    //     $this->assertTrue(true);
-    // }
->>>>>>> parent of 836c162 (fix remove tests)
 }
